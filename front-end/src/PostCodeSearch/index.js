776182e7edde52a71452707bf6b518ca36c13973
Paul Abraham <abrahamspaa@gmail.com>
2020-03-31 16:13:09 +0530

    Validation and submit validation check

--- front-end/src/PostCodeSearch/index.js
+++ front-end/src/PostCodeSearch/index.js
@@ -7,27 +7,55 @@ export default class PostCodeSearch extends React.Component {
     super(props);
 
     this.state = {
-      postCode: ''
+      postCode: '',
+      postCodeError: false
     };
   }
+
+  isPostCodeValid = postCode => new RegExp('^[1-9][0-9]{5}$').test(postCode);
 
   handleChange(event) {
     const { name, value } = event.target;
 
-    this.setState({
-      [name]: value
-    });
+    if (this.isPostCodeValid(value)) {
+      this.setState({
+        [name]: value
+      });
+    } else {
+      this.setState({ 
+        postCodeError: true
+      });
+    }
+    
+  }
+
+  onSubmit(event) {
+
+    if (this.isPostCodeValid(this.state.postCode)) {
+      // Code for ajax
+    } else {
+      this.setState({ 
+        postCodeError: true
+      });
+    }
+
+    event.preventDefault();
   }
 
   render() {
     return (
-      <form>
-        <FormGroup label="Postcode">
+      <form onSubmit={event => this.onSubmit(event)}>
+        <FormGroup label="Postcode" className={ this.state.postCodeError ? 'has-error' : '' }>
           <Input 
             name="postCode"
             placeholder="Ex: 6000056" 
             value={this.state.postCode} 
             onChange={event => this.handleChange(event)} />
+          { 
+            this.state.postCodeError ? 
+              <p className='form-input-hint'>Please enter valid post code</p> : 
+              '' 
+          }
         </FormGroup>
 
         <Button primary loading={false}>Search</Button>
