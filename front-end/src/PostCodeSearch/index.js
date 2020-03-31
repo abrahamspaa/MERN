8332e275cf95446a9f2c938dda93fdaf2ff9adea
Paul Abraham <abrahamspaa@gmail.com>
2020-03-31 15:36:59 +0530

    Frontend Postcode components fields

--- front-end/src/PostCodeSearch/index.js
+++ front-end/src/PostCodeSearch/index.js
@@ -1,7 +1,37 @@
+import { FormGroup, Input } from '@react-spectre/form';
+import { Button } from '@react-spectre/button';
 import React from 'react';
 
 export default class PostCodeSearch extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      postCode: ''
+    };
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
-    return (<h2>Now you can see PostCodeComponent</h2>);
+    return (
+      <form>
+        <FormGroup label="Postcode">
+          <Input 
+            name="postCode"
+            placeholder="Ex: 6000056" 
+            value={this.state.postCode} 
+            onChange={event => this.handleChange(event)} />
+        </FormGroup>
+
+        <Button primary loading={false}>Search</Button>
+      </form>
+    );
   }
 }
