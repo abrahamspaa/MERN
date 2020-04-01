8fc2505c4d6ac176e5094c2de79402aafbeb33c8
Paul Abraham <abrahamspaa@gmail.com>
2020-04-01 10:25:33 +0530

    Backend hard coded data populated for post

--- back-end/routes/postcode/index.js
+++ back-end/routes/postcode/index.js
@@ -1,7 +1,46 @@
 const router = require('express').Router();
 
-router.get('/', function(req, res, next) {
-  res.send('Post code value');
+router.get('/', function(request, respond) {
+  const postcode = request.query.postcode;
+  const postOffices = {
+    data: [
+      {
+        office: 'Iyyappanthangal',
+        type: 'Sub Office',
+        district: 'Kanchipuram',
+        state: 'Tamil Nadu'
+      },
+      {
+        office: 'Kattupakkam',
+        type: 'Branch Office',
+        district: 'Tiruvallur',
+        state: 'Tamil Nadu'
+      },
+      {
+        office: 'Poonamallee East',
+        type: 'Sub Office',
+        district: 'Tiruvallur',
+        state: 'Tamil Nadu'
+      },
+      {
+        office: 'Senneerkuppam',
+        type: 'Branch Office',
+        district: 'Tiruvallur',
+        state: 'Tamil Nadu'
+      }
+    ]
+  };
+
+  if (postcode === '600056') {
+    return respond.send(postOffices);
+  } else {
+    return respond.status(404).send({
+      error: {
+        message: `The ${postcode} not Found..!`
+      }
+    });    
+  }
+  
 });
 
 module.exports = router;
